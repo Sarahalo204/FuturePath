4d97f662495f4a4ab8f7c8e35f82d59a1881fc40
sarah <sarol4359@gmail.com>
2026-03-08 20:17:54 +0300

    fix: clean .next from git and update .gitignore

--- .next/dev/server/chunks/ssr/_next-internal_server_app_[locale]_admin_students_page_actions_e3cab2d5.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/[locale]/admin/students/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%5Blocale%5D_admin_students_page_actions_e3cab2d5.js.map

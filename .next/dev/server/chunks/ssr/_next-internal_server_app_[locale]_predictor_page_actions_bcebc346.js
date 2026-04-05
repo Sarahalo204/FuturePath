7c08636d93c67f09af7fb1a9727e305c8be18db8
sarah <sarol4359@gmail.com>
2026-04-05 19:47:58 +0300

    Remove .next from git tracking

--- .next/dev/server/chunks/ssr/_next-internal_server_app_[locale]_predictor_page_actions_bcebc346.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/[locale]/predictor/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_%5Blocale%5D_predictor_page_actions_bcebc346.js.map
